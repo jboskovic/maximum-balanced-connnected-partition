1c0562aca0a1793e9b6e278263c04cbcb3a28e21
jboskovic <jboskovic97@gmail.com>
2020-08-16 16:45:31 +0200

    Organizovana main funkcija

--- organized/main.py
+++ organized/main.py
@@ -5,21 +5,45 @@ from greedy_algorithm import GreedyAlgorithm
 
 
 def print_solution(solution):
+    print('---------------------------------')
     print('Solution: ')
     print('Graph1 - ', solution.subgraph1.adj)
     print('Graph2 - ', solution.subgraph2.adj)
     print('Fitness - ', solution.fitness)
     print('Code - ', solution.code)
+    print('---------------------------------')
+    print()
 
 
-if __name__ == "__main__":
-    # parametri genetskog algoritma
+def brute_force(graph):
+    print('Brute-force: ')
+    brute_force = BruteForce(graph)
+    solution = brute_force.brute_force()
+    print_solution(solution)
 
+
+def greedy(graph):
+    print('Greedy: ')
+    greedy_algorithm = GreedyAlgorithm(graph)
+    solution = greedy_algorithm.greedy_algorithm()
+    print_solution(solution)
+
+
+def genetic(graph):
+    print('Genetic: ')
     nTournament = 6
     probability = 0.05
     nPopulation = 100
     nIterations = 500
     nElite = 30
+
+    genetic = GeneticAlgorithm(
+        nPopulation, nIterations, nElite, nTournament, probability, graph)
+    solution = genetic.genetic_algorithm()
+    print_solution(solution)
+
+
+if __name__ == "__main__":
 
     # primer 1
     adjacency_list = {0: [1, 2, 5], 1: [0, 5, 2], 2: [
@@ -27,42 +51,28 @@ if __name__ == "__main__":
     vertics_cost = {0: 8, 1: 4, 2: 1, 3: 6, 4: 5, 5: 4}
     graph = Graph(adjacency_list, vertics_cost)
 
-    # genetski
-    genetic = GeneticAlgorithm(
-        nPopulation, nIterations, nElite, nTournament, probability, graph)
-    solution = genetic.genetic_algorithm()
-    print_solution(solution)
-
-    # brute-force
-    brute_force = BruteForce(graph)
-    solution = brute_force.brute_force()
-    print_solution(solution)
+    # brute force
+    brute_force(graph)
 
     # greedy
-    greedy_algorithm = GreedyAlgorithm(graph)
-    solution = greedy_algorithm.greedy_algorithm()
-    print_solution(solution)
+    greedy(graph)
+
+    # genetic
+    genetic(graph)
 
     # primer 2
     adjacency_list = {0: [1, 2], 1: [0, 4], 2: [0, 3], 3: [2, 4], 4: [1, 3]}
     vertics_cost = {0: 4, 1: 7, 2: 1, 3: 4, 4: 9}
     graph = Graph(adjacency_list, vertics_cost)
 
-    # genetski
-    genetic = GeneticAlgorithm(
-        nPopulation, nIterations, nElite, nTournament, probability, graph)
-    solution = genetic.genetic_algorithm()
-    print_solution(solution)
-
-    # brute-force
-    brute_force = BruteForce(graph)
-    solution = brute_force.brute_force()
-    print_solution(solution)
+    # brute force
+    brute_force(graph)
 
     # greedy
-    greedy_algorithm = GreedyAlgorithm(graph)
-    solution = greedy_algorithm.greedy_algorithm()
-    print_solution(solution)
+    greedy(graph)
+
+    # genetic
+    genetic(graph)
 
     # # primer 3
     vertics_cost = {0: 3, 1: 2, 2: 5, 3: 1, 4: 4, 5: 8, 6: 2, 7: 3, 8: 2, 9: 4}
@@ -70,21 +80,14 @@ if __name__ == "__main__":
         1, 3], 5: [2, 6, 9], 6: [0, 2, 5, 9], 7: [2, 9], 8: [3], 9: [6, 7]}
     graph = Graph(adjacency_list, vertics_cost)
 
-    # genetski
-    genetic = GeneticAlgorithm(
-        nPopulation, nIterations, nElite, nTournament, probability, graph)
-    solution = genetic.genetic_algorithm()
-    print_solution(solution)
-
-    # brute-force
-    brute_force = BruteForce(graph)
-    solution = brute_force.brute_force()
-    print_solution(solution)
+    # brute force
+    brute_force(graph)
 
     # greedy
-    greedy_algorithm = GreedyAlgorithm(graph)
-    solution = greedy_algorithm.greedy_algorithm()
-    print_solution(solution)
+    greedy(graph)
+
+    # genetic
+    genetic(graph)
 
     # Primer 4
     vertics_cost = {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 6, 7: 5, 8: 4, 9: 1, 10: 2, 11: 3, 12: 3, 13: 2,
@@ -93,17 +96,11 @@ if __name__ == "__main__":
         14, 12], 16: [22, 23, 17, 9], 17: [16, 10], 18: [19, 11, 27, 13], 19: [18, 27, 4, 31, 26], 20: [1, 9], 21: [28, 22], 22: [29, 21, 16], 23: [16, 9, 24, 30], 24: [23, 31, 2], 25: [28, 11, 29], 26: [19, 31], 27: [14, 18, 19], 28: [21, 1, 25], 29: [25, 22, 9], 30: [23, 9], 31: [19, 26, 24]}
     graph = Graph(adjacency_list, vertics_cost)
 
-    # greedy
-    greedy_algorithm = GreedyAlgorithm(graph)
-    solution = greedy_algorithm.greedy_algorithm()
-    print_solution(solution)
-    # # genetski
-    # genetic = GeneticAlgorithm(
-    #     nPopulation, nIterations, nElite, nTournament, probability, graph)
-    # solution = genetic.genetic_algorithm()
-    # print_solution(solution)
+    # brute force
+    # brute_force(graph)
 
-    # # brute-force
-    # brute_force = BruteForce(graph)
-    # solution = brute_force.brute_force()
-    # print_solution(solution)
+    # greedy
+    greedy(graph)
+
+    # genetic
+    genetic(graph)
